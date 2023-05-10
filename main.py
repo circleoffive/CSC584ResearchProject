bad75835dcaa5bbe0b84bfdb3ab0dbf99d2f40a6
Andrew Asendorf <aasendorf1@toro.csudh.edu>
2023-05-10 15:27:29 -0700

    new files and changed files

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # CSC 584 Research Project
 def print_hi(name):
-    print(f'My name is , {name}')
+    print(f'My name is, {name}')
 
 
 if __name__ == '__main__':
     print('Hello World!')
-
+    print_hi('Andrew')
 
